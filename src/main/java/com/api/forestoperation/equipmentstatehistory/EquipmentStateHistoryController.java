3edc1c135f353b5cb18a06858244065bceefb197
RobertoCassio <rcslino@gmail.com>
2023-12-10 22:08:12 -0300

    Added save function on Controller on  EquipmentStateHistory

--- src/main/java/com/api/forestoperation/equipmentstatehistory/EquipmentStateHistoryController.java
+++ src/main/java/com/api/forestoperation/equipmentstatehistory/EquipmentStateHistoryController.java
@@ -1,8 +1,21 @@
 package com.api.forestoperation.equipmentstatehistory;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EquipmentStateHistoryController {
+	@Autowired
+	EquipmentStateHistoryService equipmentStateHistory;
+	
+	@PostMapping("/equipment-state-history")
+	public ResponseEntity<Object> saveEquipmentStateHistory(EquipmentStateHistoryDTO equipmentStateHistoryDTO){
+		var savedEquipmentStateHistory = equipmentStateHistory.createStateHistory(equipmentStateHistoryDTO);
+		return savedEquipmentStateHistory != null ? ResponseEntity.status(HttpStatus.OK).body("EquipmentStateHistory adicionado com sucesso") : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
 
 }
